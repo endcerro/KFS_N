2e4e94434d76d82f3e92450b081cc1de22404156
Enzo Dal Cerro <edal--ce@student.42.fr>
2024-07-17 21:06:55 +0200

    1

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,11 @@
 #![no_std]
 #![no_main]
-
-// use multiboot2::{BootInformation, BootInformationHeader};
-
 #[macro_use]
 pub mod vga;
-// pub mod interrupts;
-// pub mod multiboot;
-// pub mod gdt;
 use core::panic::PanicInfo;
 
 #[no_mangle]
-// pub extern "C" fn rust_main(multiboot_struct_ptr: *const multiboot::MultibootInfoHeader, _multiboot_magic_nbr : usize  ) -> ! {
-pub extern "C" fn rust_main(multiboot_struct_ptr: usize, _multiboot_magic_nbr : usize  ) -> ! {
+pub extern "C" fn rust_main(_multiboot_struct_ptr: usize, _multiboot_magic_nbr : usize) -> ! {
     init();
     loop {}
 }
