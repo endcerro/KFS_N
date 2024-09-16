6a0ade256d108d14283de94836baafa058be3d94
Victor PORTENSEIGNE <viporten@paul-f5Ar3s4.clusters.42paris.fr>
2024-09-16 18:02:29 +0200

    fix cmd echo without arg

--- src/commands/echo.rs
+++ src/commands/echo.rs
@@ -1,4 +1,5 @@
 pub fn run(args: &str) {
-    let output : &str = &args[5..];
-    println!("\n{}", output);
+    let output : &str = args.get(5..).unwrap_or("");
+    
+    print!("\n{}", output);
 }
