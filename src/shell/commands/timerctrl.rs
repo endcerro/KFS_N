f50244ef6df3c4c435682f41eea1268ed8167225
Enzo DAL  CERRO <edal--ce@student.42.fr>
2026-03-25 17:42:30 +0100

    clean

--- src/shell/commands/timerctrl.rs
+++ src/shell/commands/timerctrl.rs
@@ -1,23 +1,6 @@
 use crate::timer::{ACTIVE_MODES, MODE_COUNTER, MODE_HEARTBEAT, MODE_UPTIME};
 use core::sync::atomic::Ordering;
-// pub fn run(args: &[&str]) {
-//     match args.first() {
-//         Some(&"on") => crate::timer::enable(),
-//         Some(&"off") => crate::timer::disable(),
-//         Some(&"status") => {
-//             let state = if crate::timer::is_enabled() {
-//                 "ON"
-//             } else {
-//                 "OFF"
-//             };
-//             println!("\nTimer: {}  Ticks: {}", state, crate::timer::get_ticks());
-//         }
-//         _ => {
-//             println!("");
-//             println!("\nUsage: timer on|off|status");
-//         }
-//     }
-// }
+
 pub fn run(args: &[&str]) {
     match args.first() {
         Some(&"on") => crate::timer::enable(),
